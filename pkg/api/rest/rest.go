b495d4c50b86a00aa230ec6d131e68604047d6bd
Ilya Stakheev <istaheev@users.noreply.github.com>
2020-04-02 10:42:14 +0200

    Add simple get/set functionality

--- pkg/api/rest/rest.go
+++ pkg/api/rest/rest.go
@@ -2,32 +2,57 @@ package rest
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"unicode/utf8"
 
 	"github.com/gorilla/mux"
 	"github.com/istaheev/kvstore-assignment/pkg/kvstore"
 )
 
 type keyGetHandler struct {
+	kvs *kvstore.KeyValueStore
 }
 
 func (h keyGetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var key = mux.Vars(r)["key"]
-	w.Write([]byte(fmt.Sprintf("Requested key '%s'", key)))
+	key := mux.Vars(r)["key"]
+	value, ok := h.kvs.Get(key)
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(value))
+	}
 }
 
 type keySetHandler struct {
+	kvs *kvstore.KeyValueStore
 }
 
 func (h keySetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var key = mux.Vars(r)["key"]
-	w.Write([]byte(fmt.Sprintf("About to set key '%s'", key)))
+	key := mux.Vars(r)["key"]
+	valueArr, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("Request body read error: %s", err.Error())))
+		return
+	}
+
+	if !utf8.Valid(valueArr) {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Request body is not valid UTF-8"))
+		return
+	}
+
+	value := string(valueArr)
+	h.kvs.Set(key, value)
+	w.Write([]byte("OK"))
 }
 
 // New initializes REST-like API
 func New(kvs *kvstore.KeyValueStore) http.Handler {
 	var router = mux.NewRouter().StrictSlash(true)
-	router.Handle("/{key}", keyGetHandler{}).Methods(http.MethodGet)
-	router.Handle("/{key}", keySetHandler{}).Methods(http.MethodPost)
+	router.Handle("/{key}", keyGetHandler{kvs}).Methods(http.MethodGet)
+	router.Handle("/{key}", keySetHandler{kvs}).Methods(http.MethodPost)
 	return router
 }
--- pkg/kvstore/kvstore.go
+++ pkg/kvstore/kvstore.go
@@ -2,9 +2,25 @@ package kvstore
 
 // KeyValueStore is a place where key/value pairs can be stored
 type KeyValueStore struct {
+	kvmap map[string]string
+}
+
+// Get retrieves a key from the store.
+// It returns the key's value (empty if not found) and a boolean value telling
+// if the key was found in the store
+func (kvs *KeyValueStore) Get(key string) (string, bool) {
+	value, ok := kvs.kvmap[key]
+	return value, ok
+}
+
+// Set sets a key and its value in the store.
+func (kvs *KeyValueStore) Set(key, value string) {
+	kvs.kvmap[key] = value
 }
 
 // New creates new empty key/value store
 func New() KeyValueStore {
-	return KeyValueStore{}
+	return KeyValueStore{
+		kvmap: make(map[string]string),
+	}
 }
